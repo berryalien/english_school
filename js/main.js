8cb111b20722561ee4eccc60ecb13375108afc81
Christina <150205323+berryalien@users.noreply.github.com>
2024-06-03 22:50:05 +0400

    site adaptive + interactive map + sticky head + burger menu js

--- js/main.js
+++ js/main.js
@@ -9,24 +9,49 @@ $(function(){
     }, 2500);
   });
 
-  $('.menu__btn, .menu a').click(function () {
-    $('.menu__btn, .menu__list').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
+var ESC_KEYCODE = 27;
+var body = document.querySelector('body');
+var headerOpen = document.querySelector('#button-open');
+var headerClose = document.querySelector('#button-close');
 
+function closeHeaderMenu() {
+  headerOpen.addEventListener('click', openHeaderMenu);
+  headerClose.removeEventListener('click', closeHeaderMenu);
+  document.removeEventListener('keydown', onEscPress);
+  body.classList.add('closed-menu-js');
+  body.classList.remove('opened-menu-js');
+}
 
-  var header = $(".header__top");
-  var scrollChange = 30;
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
+function openHeaderMenu() {
+  headerOpen.removeEventListener('click', openHeaderMenu);
+  headerClose.addEventListener('click', closeHeaderMenu);
+  document.addEventListener('keydown', onEscPress);
+  body.classList.remove('closed-menu-js');
+  body.classList.add('opened-menu-js');
+}
 
-    if (scroll >= scrollChange) {
-      header.addClass('header-fixed');
-    } else {
-      header.removeClass("header-fixed");
-    }
+function onEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeHeaderMenu();
+  }
+}
 
-  });
+closeHeaderMenu();
+
+let header = document.querySelector('.js-header'),
+    headerH = document.querySelector('.js-header').clientHeight;
+
+document.onscroll = function () {
+  let scroll = window.scrollY;
+
+  if (scroll > headerH) {
+    header.classList.add('fixed');
+    document.body.style.paddingTop = headerH + 'px';
+  } else {
+    header.classList.remove('fixed');
+    document.body.removeAttribute('style');
+  } 
+}
 
 
 $('.slider__list').slick({
@@ -37,11 +62,23 @@ $('.slider__list').slick({
     speed: 300,
     adaptiveHeight: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/teachers/icon/slick-prev.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/teachers/icon/slick-next.svg" alt=""></button>'
-
+    nextArrow: '<button type="button" class="slick-next"><img src="images/teachers/icon/slick-next.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        }, 
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+        
 });
-
-
-
-
-  });
+});
